5f0821809adb2c62ebf57e816f076ab0805c128c
Joe Monem <jmonem@icloud.com>
2022-08-12 16:10:46 +0300

    clippy

--- packages/common/src/ado_base/mod.rs
+++ packages/common/src/ado_base/mod.rs
@@ -70,7 +70,7 @@ pub enum AndromedaQuery {
 }
 
 /// Helper enum for serialization
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq, JsonSchema, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     AndrReceive(AndromedaMsg),
