fe14d24d0601d2004f12f40c0902dbedf11d9e68
Joe Monem <jmonem@icloud.com>
2022-08-12 15:25:41 +0300

    clippy fix

--- packages/common/src/ado_base/mod.rs
+++ packages/common/src/ado_base/mod.rs
@@ -21,7 +21,7 @@ pub struct InstantiateMsg {
     pub primitive_contract: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq, JsonSchema, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum AndromedaMsg {
     /// Standard Messages
